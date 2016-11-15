6d578a2d5bc36569477ee06a465a89113829107b
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-15 11:11:15 -0500

    Initial Commit

---
+++ EveryDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDo
+//
+//  Created by Hamza Lakhani on 2016-11-15.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Hamza Lakhani on 2016-11-15.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
